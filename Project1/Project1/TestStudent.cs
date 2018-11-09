d0d817e7ca7b5cb694c33bcc8696a5e3b5b9a6c0
Trong Nguyen <nguytk01@students.ipfw.edu>
2018-11-09 14:18:50 -0500

    appveyor

--- Project1/Project1/TestStudent.cs
+++ Project1/Project1/TestStudent.cs
@@ -6,33 +6,54 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using System.Runtime.Serialization.Json;
 using System.Net.Http;
+using System.Diagnostics;
+using System.IO;
 
 namespace Project1
 {
-    
     [TestFixture]
     public class TestStudent
     {
         string receivedSessionKey;
-        const string serverIP = "127.0.0.1:8000";
+        const string serverIP = "127.0.0.1";
+        const string serverPort = "8000";
+        Process server;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            Console.WriteLine(Path.Combine(
+            Directory.GetParent(Directory.GetCurrentDirectory()).FullName,
+            "server.exe"));
+
+            server = Process.Start(
+            Path.Combine(
+            Directory.GetParent(Directory.GetCurrentDirectory()).FullName,
+            "server.exe"));
+
+            TestContext.Out.WriteLine("server ID : " + server.Id);
+        }
 
         [TestCase]
         public void TestServer(){
             request().Wait();
             byte[] arr = Convert.FromBase64String(receivedSessionKey);
             Assert.AreEqual(30, arr.Length);
-
         }
 
         public async Task request(){
             HttpClient httpClient = new HttpClient();
             DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(SessionType));
-            var content = await httpClient.GetAsync("http://127.0.0.1:8000/newUnauthorizedSession");
+            var content = await httpClient.GetAsync("http://" + serverIP + ":"+ serverPort+"/newUnauthorizedSession");
             System.IO.Stream stream = await content.Content.ReadAsStreamAsync();
             SessionType obj = (SessionType)ser.ReadObject(stream);
             receivedSessionKey = obj.Session;
         }
 
-        
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            server.Kill();
+        }
     }
 }
